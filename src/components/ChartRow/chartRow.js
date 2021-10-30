c02bab21e198d2388e7e7c2c967f8d57cb06bc1f
Ori <oorishema@gmail.com>
2021-10-30 19:49:50 +0300

    Add total score

--- src/components/ChartRow/chartRow.js
+++ src/components/ChartRow/chartRow.js
@@ -18,13 +18,19 @@ function ChartRow({ companyData }) {
     const [open, setOpen] = useState(false)
     const [isMultiLeads, setIsMultiLeads] = useState(false)
     const [multiLeadsPreview, setMultiLeadsPreview] = useState("")
+    const [totalScore, setTotalScore] = useState("")
 
     useEffect(() => {
+        companyData.rounds.map(round => {
+            round.IGR = IGRcaculation(round.multiplier, round.TSLI);
+        })
         const leadInvestors = companyData.rounds[companyData.rounds.length - 1].leadInvestor;
         checkMultiLeadInvestors(leadInvestors);
         caculateTVR();
         caculateFinance();
-    }, [])
+        caculateTeamScore();
+        caculateFinalScore();
+    }, [companyData])
 
     const checkMultiLeadInvestors = (leadInvestors) => {
         let leadsArray = leadInvestors.split(",")
@@ -45,7 +51,10 @@ function ChartRow({ companyData }) {
             if (avgTSLIforRound < 7) companyData.rounds[i].TVR = 10;
             else if (avgTSLIforRound > 23) companyData.rounds[i].TVR = 0;
             // caculate each round avg TSLI(6-24) and return points between 0-10
-            else companyData.rounds[i].TVR = 10 - ((avgTSLIforRound - 6) * 0.555)
+            else {
+                const TVRscore = 10 - ((avgTSLIforRound - 6) * 0.555)
+                companyData.rounds[i].TVR = Math.round(TVRscore * 100) / 100
+            }
         }
     }
 
@@ -75,13 +84,100 @@ function ChartRow({ companyData }) {
                     if (financeTotal > 250) companyData.rounds[i].finance = 5;
                     break;
                 default:
+                    companyData.rounds[i].finance = 0;
                     break;
             }
         }
     }
 
+    const IGRcaculation = (multiplier, TSLI) => {
+        let IGRscore = "N/A"
+        if (TSLI && multiplier) {
+            let IGRrawScore = multiplier / TSLI;
 
+            let refundingTimingFactor = 3;
+            if (TSLI <= 5) console.log("↓↓↓ Need Checking IGR ↓↓↓")
 
+            else if (TSLI <= 12) {
+                //reducing 0.085 factor value for every TSLI-month above 5. if TSLI = 8 then reducing 0.255(0.085 x 3) from the base factor of 3 => 2.745
+                refundingTimingFactor -= ((TSLI - 5) * 0.085);
+            }
+            else if (TSLI <= 24) {
+                //above 1 year start with 0.6 less factor value
+                refundingTimingFactor -= 0.6;
+                //reducing 0.11 factor value for every TSLI-month above 12. if TSLI = 14 then reducing 0.220(0.11 x 2) from the base factor of 2.4 => 2.180
+                refundingTimingFactor -= ((TSLI - 12) * 0.11);
+            }
+            else refundingTimingFactor = 1;
+
+            IGRscore = IGRrawScore * refundingTimingFactor;
+            IGRscore = Math.round(IGRscore * 100) / 100;
+        }
+        IGRscore = IGRvalueCac(IGRscore)
+        IGRscore = Math.round(IGRscore * 100) / 100
+        return IGRscore
+    }
+
+    const IGRvalueCac = (IGR) => {
+        //Here we detremine the value of IGR score and shift it to score between 0-25points (25% from final score)
+        if (IGR < 0.04) IGR = 1;
+        else if (IGR >= 0.04 && IGR <= 0.5) {
+            // 15 points passible divided to 46 (0.46 points of value )
+            let measureValue = 15 / 46
+            IGR = (IGR - 0.04) * 100 * measureValue
+        }
+        else if (IGR > 0.5) {
+            // Another 10 points - 1 points for each 0.1 above 0.5
+            if (IGR > 1.5) IGR = 25;
+            else IGR = ((IGR - 0.5) * 10) + 15;
+        }
+        return IGR;
+    }
+
+    const caculateTeamScore = () => {
+        const rankValue = {
+            CEOandLead: {
+                AAA: 21.84,
+                AA: 16.38,
+                A: 10.92,
+                B: 5.46
+            },
+            previousLead: {
+                AAA: 16.32,
+                AA: 12.24,
+                A: 8.16,
+                B: 4.08
+            }
+        }
+        companyData.CEOscore ?
+            companyData.CEOscore = rankValue.CEOandLead[companyData.CEOscore]
+            :
+            companyData.CEOscore = 0;
+        companyData.leadScore ?
+            companyData.leadScore = rankValue.CEOandLead[companyData.leadScore]
+            :
+            companyData.leadScore = 0;
+        companyData.previousLeadScore ?
+            companyData.previousLeadScore = rankValue.previousLead[companyData.previousLeadScore]
+            :
+            companyData.previousLeadScore = 0;
+    }
+
+    //TODO: 1 - FIX FINANCE |  2 - FIX SCORE SORT 
+    const caculateFinalScore = () => {
+        const lastRound = companyData.rounds[companyData.rounds.length - 1];
+        const finalIGR = lastRound.IGR;
+        const finalTVR = lastRound.TVR;
+        const finalFinance = lastRound.finalfinance ? lastRound.finance : 0
+        const { CEOscore } = companyData;
+        const { leadScore } = companyData;
+        const { previousLeadScore } = companyData
+        const score = finalIGR + finalTVR + finalFinance + CEOscore + leadScore + previousLeadScore;
+        const finalScore = Math.round(score * 100) / 100
+        companyData.score = finalScore;
+        setTotalScore(finalScore);
+
+    }
 
     return (
         <>
@@ -118,7 +214,7 @@ function ChartRow({ companyData }) {
                     </TableCell>
                 }
                 <TableCell align="left">{companyData.CEO}</TableCell>
-                <TableCell align="center">not yet</TableCell>
+                <TableCell align="center">{totalScore && totalScore}</TableCell>
             </TableRow>
             <TableRow >
                 <>
@@ -127,7 +223,7 @@ function ChartRow({ companyData }) {
                             <div style={{ display: "flex", flexWrap: "wrap" }}>
                                 {companyData.rounds.map(round => (
                                     <div key={round.roundNumber}>
-                                        <CompanyRoundInfo roundInfo={round} allRoundsInfo={companyData.rounds} />
+                                        <CompanyRoundInfo roundInfo={round} CEOscore={companyData.CEOscore} leadScore={companyData.leadScore} preLeadScore={companyData.previousLeadScore} />
                                     </div>
                                 ))}
                             </div>
--- src/components/CompanyRoundInfo/companyRoundInfo.js
+++ src/components/CompanyRoundInfo/companyRoundInfo.js
@@ -7,7 +7,7 @@ import GroupIcon from '@mui/icons-material/Group';
 import { Button, Tooltip } from '@material-ui/core';
 
 
-function CompanyRoundInfo({ roundInfo, allRoundsInfo }) {
+function CompanyRoundInfo({ roundInfo, CEOscore, leadScore, preLeadScore }) {
 
     const [isMultiLeads, setIsMultiLeads] = useState(false)
     const [multiLeadsPreview, setMultiLeadsPreview] = useState("")
@@ -18,10 +18,7 @@ function CompanyRoundInfo({ roundInfo, allRoundsInfo }) {
         const leadInvestors = roundInfo.leadInvestor
         const investors = roundInfo.investors
         checkMultiInvestors(leadInvestors, investors)
-        let IGRscore = IGRcaculation(roundInfo.multiplier, roundInfo.TSLI)
-        roundInfo.IGR = IGRscore;
         roundInfo.TVR = Math.round(roundInfo.TVR * 100) / 100
-        console.log(roundInfo)
     }, [roundInfo])
 
     const checkMultiInvestors = (leadInvestors, investors) => {
@@ -41,49 +38,7 @@ function CompanyRoundInfo({ roundInfo, allRoundsInfo }) {
         }
     }
 
-    const IGRcaculation = (multiplier, TSLI) => {
-        let IGRscore = "N/A"
-        if (TSLI && multiplier) {
-            let IGRrawScore = multiplier / TSLI;
 
-            let refundingTimingFactor = 3;
-            if (TSLI <= 5) console.log("↓↓↓ Need Checking IGR ↓↓↓")
-
-            else if (TSLI <= 12) {
-                //reducing 0.085 factor value for every TSLI-month above 5. if TSLI = 8 then reducing 0.255(0.085 x 3) from the base factor of 3 => 2.745
-                refundingTimingFactor -= ((TSLI - 5) * 0.085);
-            }
-            else if (TSLI <= 24) {
-                //above 1 year start with 0.6 less factor value
-                refundingTimingFactor -= 0.6;
-                //reducing 0.11 factor value for every TSLI-month above 12. if TSLI = 14 then reducing 0.220(0.11 x 2) from the base factor of 2.4 => 2.180
-                refundingTimingFactor -= ((TSLI - 12) * 0.11);
-            }
-            else refundingTimingFactor = 1;
-
-            IGRscore = IGRrawScore * refundingTimingFactor;
-            IGRscore = Math.round(IGRscore * 100) / 100;
-        }
-        IGRscore = IGRvalueCac(IGRscore)
-        IGRscore = Math.round(IGRscore * 100) / 100
-        return IGRscore
-    }
-
-    const IGRvalueCac = (IGR) => {
-        //Here we detremine the value of IGR score and shift it to score between 0-25points (25% from final score)
-        if (IGR < 0.04) IGR = 1;
-        else if (IGR >= 0.04 && IGR <= 0.5) {
-            // 15 points passible divided to 46 (0.46 points of value )
-            let measureValue = 15 / 46
-            IGR = (IGR - 0.04) * 100 * measureValue
-        }
-        else if (IGR > 0.5) {
-            // Another 10 points - 1 points for each 0.1 above 0.5
-            if (IGR > 1.5) IGR = 25;
-            else IGR = ((IGR - 0.5) * 10) + 15;
-        }
-        return IGR;
-    }
 
     return (
         <>
@@ -133,11 +88,11 @@ function CompanyRoundInfo({ roundInfo, allRoundsInfo }) {
                 </div>
                 <div className="scoreData">
                     <div>{`IGR Score: ${roundInfo.IGR}`}</div>
-                    <div>TVR Score: {roundInfo.TVR}</div>
-                    <div>Finance Score: {roundInfo.Finance}</div>
-                    <div>CEO score</div>
-                    <div>Lead Score</div>
-                    <div>Previous Lead Score</div>
+                    <div>{`TVR Score: ${roundInfo.TVR}`}</div>
+                    <div>{`Finance Score: ${roundInfo.finance}`}</div>
+                    <div>{`CEO score: ${CEOscore}`}</div>
+                    <div>{`Lead Score ${leadScore}`}</div>
+                    <div>{`Previous Lead Score ${preLeadScore}`}</div>
                 </div>
             </div>
         </>
